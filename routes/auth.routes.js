1d5f465b9fabcefcc2c07bebe669d38532154552
Jesse Vermeulen <jessevermeulen123@gmail.com>
2021-09-07 16:02:31 +0200

    bycrypts

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,17 @@
 
 const {Router} = require('express');
 const router = new Router();
+const User = require('../models/User.model');
 
 //Get route => to display the signup form
 router.get("/signup", (req, res) => {
   res.render("auth/signup")
 })
 
+router.get('/userProfile', (req, res)=>{
+  res.render('users/user-profile')
+  //User.findById(id)
+})
 
 
 //Post route => to process the data
@@ -20,6 +25,14 @@ router.post("/signup", (req, res)=>{
   bcryptjs
   .genSalt(saltRounds)
   .then(salt => bcryptjs.hash(password, salt))
+  .then(hashedPassword =>{
+    return User.create({username, email, passwordHash: hashedPassword})
+    .then(userFromDB => { 
+      res.redirect('/userProfile')
+    })
+    .catch(error=> console.log('user was not created'))
+  })
+  .catch(error => console.log(error))
   
 })
 
